05e8929f613451044d966cad29648f4122638e2d
Aditya Chezhiyan <adityachezhiyan@169-231-52-195.wireless.ucsb.edu>
2024-06-06 19:56:31 -0700

    Updating data collection

--- geiger_counter/DataWriter.py
+++ geiger_counter/DataWriter.py
@@ -8,28 +8,42 @@ Created on Thu May  2 20:32:21 2024
 
 import serial
 from datetime import datetime
-import atexit
+from scipy.stats import expon
+import numpy as np
 
 #Port used for the Arduino (check with Arduino IDE/Command Line)
 port = "/dev/cu.usbmodem101"
 #Notes added to the file name, change as needed
-notes = "Sr90_10cm_425V"
+notes = "new_data_test"
 file_name = ("count_times_%s_%s.txt" % (str(datetime.now()).replace(":", ""), notes))
+
+#Returns counts rate (per second) and its error
+#This requires that the environment does not change over the measurement period
+def rate(ds):
+    r = 1 / expon.fit(ds, floc=0, scale=np.average(ds))[1]
+    return r, r / (len(ds) ** 0.5)
 
 #Measurement period in seconds
 period = 180
 
-def exit_handler():
+deltas = []
+total = 0
+ser = serial.Serial(port, 9600)
+while True:
+    time = int.from_bytes(ser.read(size=4), "little") / 1000000.0
+    deltas.append(time)
+    print(time)
+    total += time
+    if total > period:
+        break
     ser.flush()
-    ser.close()
-atexit.register(exit_handler)
 
+ser.flush()
+ser.close()
 with open(file_name, "w") as output_file:
-    ser = serial.Serial(port, 9600)
-    while True:
-        time = int.from_bytes(ser.read(size=4), "little") / 1000000.0
+    r = rate(deltas)
+    print(r)
+    output_file.write(("Rate (cps): %s" % r[0]) + "\n")
+    output_file.write(("Error: %s" % r[1]) + "\n")
+    for time in deltas:
         output_file.write(str(time) + "\n")
-        print(time)
-        if time > period:
-            break
-        ser.flush()
--- geiger_counter/Display/Display.ino
+++ geiger_counter/Display/Display.ino
@@ -1,15 +1,9 @@
 #include <LiquidCrystal.h>
 
-//Size*Period is the sampling window, change as needed
-const int size = 20;
-//Period is how often the display updates, change as needed
-const int period = 100;
-int i = 0;
-volatile long current;
-long counter[size];
-long count;
+//Update frequency, in ms
+const int period = 1000;
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
-const long t0 = micros();
+unsigned long t0 = micros();
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,47 +13,19 @@ void setup() {
 
   lcd.begin(16, 2);
   lcd.print("CountsPerMinute:");
-
-  for(int k = 0; k < size; k++) {
-    delay(period);
-    cli();
-    counter[i] = current;
-    current = 0;
-    count = 0;
-    for(int j = 0; j <= k; j++) {
-      count += counter[j];
-    }
-    //Serial.println(60 * 1000.0 * count / ((k+1) * period));
-    lcd.setCursor(0, 1);
-    lcd.print(60 * 1000.0 * count / ((k+1) * period));
-    i = (i + 1) % size;
-    counter[i] = 0;
-    sei();
-  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(period);
-  cli();
-  counter[i] = current;
-  current = 0;
-  count = 0;
-  for(long j: counter) {
-    count += j;
-  }
-  //Serial.println(60 * 1000.0 * count / (size * period));
   lcd.setCursor(0, 1);
-  lcd.print(60 * 1000.0 * count / (size * period));
-  i = (i + 1) % size;
-  counter[i] = 0;
-  sei();
+  lcd.print(0);
 }
 
 void increment() {
-  current++;
-  unsigned long t = (micros() - t0);
+  unsigned long e = micros();
+  unsigned long t = e - t0;
+  t0 = e;
   byte buf[4] = {t & 0xFF , (t >> 8) & 0xFF, (t >> 16) & 0xFF, (t >> 24) & 0xFF};
   Serial.write(buf, 4);
-  //Serial.println(t);
 }
